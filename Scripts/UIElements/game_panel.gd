033fc9730b515688bc02bee2e0f19ead0fe15670
Doogler <elijah.ross2006@gmail.com>
2025-11-05 14:38:23 -0500

    Create game panel

---
+++ Scripts/UIElements/game_panel.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+var game_data: GameData
+
+func _ready() -> void:
+	%Button.pressed.connect(_on_pressed)
+
+func update_visuals() -> void:
+	%NameLabel.text = game_data.game_name
+	%Icon.texture = game_data.icon
+
+# TODO: Select game
+func _on_pressed():
+	pass
