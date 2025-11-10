5a7d6adf00329db596846ed57d7c630a78b87951
Doogler <elijah.ross2006@gmail.com>
2025-11-10 17:07:08 -0500

    Allow sorting games in reverse order

--- Scripts/Globals/game_organizer.gd
+++ Scripts/Globals/game_organizer.gd
@@ -2,18 +2,28 @@ extends Node
 
 var current_grouping := "favorited"
 var current_sorting := "alphabetical"
-
+var sorting_reversed := false
 
 ## Returns organized games grouped and sorted according to current methods
 func get_organized_games() -> Dictionary:
 	if current_grouping == "none":
 		# Return sorted array as one group
-		var sorted_games = SettingsManager.manager.games_library.duplicate()
-		sorted_games.sort_custom(func(a, b): return compare_games(a, b))
+		var sorted_games = get_sorted_games(SettingsManager.manager.games_library)
 		return {"All": sorted_games}
 	else:
 		# Group games and sort within groups
 		return get_grouped_and_sorted_games()
+
+
+## Sorts games in an array
+func get_sorted_games(games_array: Array) -> Array:
+	var sorted_games = games_array.duplicate()
+	sorted_games.sort_custom(func(a, b): return compare_games(a, b))
+	
+	if sorting_reversed:
+		sorted_games.reverse()
+	
+	return sorted_games
 
 
 ## Returns grouped and sorted games according to current methods
@@ -28,7 +38,7 @@ func get_grouped_and_sorted_games() -> Dictionary:
 	
 	# Sort within each group
 	for key in grouped:
-		grouped[key].sort_custom(func(a, b): return compare_games(a, b))
+		grouped[key] = get_sorted_games(grouped[key])
 	
 	# Sort the group keys themselves for consistent order
 	var sorted_keys = grouped.keys()
@@ -46,7 +56,7 @@ func compare_games(a: GameData, b: GameData) -> bool:
 	match current_sorting:
 		"alphabetical":
 			return a.game_name.to_lower() < b.game_name.to_lower() # Compare using unicode order
-		"date_added":
+		"date_created":
 			# Extract date array, formatted [month, day, year]
 			var a_date = (a.creation_date.split("/") as Array).map(func(e): return int(e))
 			var b_date = (b.creation_date.split("/") as Array).map(func(e): return int(e))
@@ -80,7 +90,8 @@ func get_group(game: GameData) -> String:
 			return "All"
 
 
-func set_sorting(sorting_type: String) -> void:
+func set_sorting(sorting_type: String, reversed := false) -> void:
+	sorting_reversed = reversed
 	current_sorting = sorting_type
 	SettingsManager.ui_manager.display_games_list()
 
